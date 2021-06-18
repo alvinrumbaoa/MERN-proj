1c8098171d41d29c5a2dbae2a095d1046369f8cd
xenodochy23 <79444246+xenodochy23@users.noreply.github.com>
2021-06-18 01:08:58 -0700

    add broken server connect to front end

--- product-manager/server/server.js
+++ product-manager/server/server.js
@@ -1,1 +1,13 @@
-const 
+const express = require("express");
+const app = express();
+const cors = require("cors");
+//import libraries for express , cors 
+require("./config/mongoose.config.js"); //  connecting to mongoose
+
+app.use(express()); 
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+require("./controller/product.controller");
+
+app.listen(8000, () => console.log("Connected to  localhost8000")); //
